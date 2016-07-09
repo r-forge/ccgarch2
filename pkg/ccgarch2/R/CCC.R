f398a28d20bf72ff45a4db51b13b9402466710e5
nakatani (Dr. ) <naktom2@gmail.com>
2016-07-09 08:06:17 +0000

    some change in NAMESPACE
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ccgarch2@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ccgarch2/R/CCC.R
+++ pkg/ccgarch2/R/CCC.R
@@ -1,23 +1,21 @@
 ####################################
 loglik.ccc.t <- function(param, data, model, mode){
-    if(is.zoo(data)) data <- as.matrix(data)
-    nobs <- dim(data)[1]
+  if(is.zoo(data)) data <- as.matrix(data)
+  nobs <- dim(data)[1]
   ndim <- dim(data)[2]
   mu <- matrix(param[1:ndim], nobs, ndim, byrow = TRUE)    # constant in the mean
   data <- data - mu
   param <- param[-(1:ndim)]
-
+  
   para.mat <- p.mat(param, model, ndim)  
   # check if R is positive definite. If not, making R positive definite
   R <- make.pd(para.mat$R)
 
-  
-  
   h <- vgarch(para.mat$a, para.mat$A, para.mat$B, data)    # a call to vgarch function
   z <- data/sqrt(h)
   invR <- solve(R)
   lf <- -0.5*ndim*log(2*pi) - 0.5*rowSums(log(h)) - 0.5*log(det(R)) - 0.5*rowSums((z%*%invR)*z)
-
+  
   if(mode=="gradient"){
     lf  
   } else {
@@ -26,17 +24,17 @@ loglik.ccc.t <- function(param, data, model, mode){
 }
 
 loglik.ccc <- function(param, data, model){
-    if(is.zoo(data)) data <- as.matrix(data)
-    nobs <- dim(data)[1]
+  if(is.zoo(data)) data <- as.matrix(data)
+  nobs <- dim(data)[1]
   ndim <- dim(data)[2]
   mu <- matrix(param[1:ndim], nobs, ndim, byrow = TRUE)    # constant in the mean
   data <- data - mu
   param <- param[-(1:ndim)]
-
+  
   para.mat <- p.mat(param, model, ndim)  
   # check if R is positive definite. If not, making R positive definite
   R <- make.pd(para.mat$R)
-
+  
   h <- vgarch(para.mat$a, para.mat$A, para.mat$B, data)    # a call to vgarch function
   z <- data/sqrt(h)
   invR <- solve(R)
@@ -50,22 +48,22 @@ make.pd <- function(x, tol=1e-6) {
   eig <- eigen(x, symmetric=TRUE)
   rtol <- tol * eig$values[1]
   if(min(eig$values) < rtol) {
-      vals <- eig$values
-      vals[vals < rtol] <- rtol
-      srev <- eig$vectors %*% (vals * t(eig$vectors))
-      dimnames(srev) <- dimnames(x)
-      srev <- diag(1/sqrt(diag(srev)))%*%srev%*%diag(1/sqrt(diag(srev)))
-      return(srev) 
+    vals <- eig$values
+    vals[vals < rtol] <- rtol
+    srev <- eig$vectors %*% (vals * t(eig$vectors))
+    dimnames(srev) <- dimnames(x)
+    srev <- diag(1/sqrt(diag(srev)))%*%srev%*%diag(1/sqrt(diag(srev)))
+    return(srev) 
   } else {return(x)}
 }
 
 # Inequality constraints for the stationarity of the vector GARCH equation in the (E)CCC GARCH
 inEQccc <- function(param, data, model){
-    ndim <- ncol(data)
-    param <- param[-(1:ndim)]   # removing constants in the mean 
-    pmat <- p.mat(param, model, ndim)
-    ret <- max(Mod(eigen(pmat$A + pmat$B)$values))     # common to diagonal/extended
-    ret
+  ndim <- ncol(data)
+  param <- param[-(1:ndim)]   # removing constants in the mean 
+  pmat <- p.mat(param, model, ndim)
+  ret <- max(Mod(eigen(pmat$A + pmat$B)$values))     # common to diagonal/extended
+  ret
 }
 
 #*****************************************************************************************************************
@@ -74,15 +72,15 @@ estimateCCC <- function(inia = NULL, iniA = NULL, iniB = NULL, data, model="diag
   ndim <- dim(data)[2]
   
   if(is.zoo(data)){
-      d.ind <- index(data)
+    d.ind <- index(data)
   } else {
-      d.ind <- 1:nobs
+    d.ind <- 1:nobs
   }
-
+  
   data <- as.matrix(data)
   mu <- colMeans(data)
   R <- cor(data)
-
+  
   # setting upper and lower bounds for the constraints
   if(model == "diagonal"){
     LB <- c(rep(-Inf, ndim), rep(0, 3*ndim), rep(-1, ndim*(ndim-1)/2))
@@ -145,18 +143,18 @@ estimateCCC <- function(inia = NULL, iniA = NULL, iniB = NULL, data, model="diag
       ntry <- ntry + 1
     }
   }
-    
-   mu <- matrix(results$pars[1:ndim], nobs, ndim, byrow = TRUE)
-   eps <- data - mu
-
-   param <- results$pars[-(1:ndim)]
-   estimates <- p.mat(param, model=model, ndim=ndim)
-   estimates$R <- make.pd(estimates$R)
-   
+  
+  mu <- matrix(results$pars[1:ndim], nobs, ndim, byrow = TRUE)
+  eps <- data - mu
+  
+  param <- results$pars[-(1:ndim)]
+  estimates <- p.mat(param, model=model, ndim=ndim)
+  estimates$R <- make.pd(estimates$R)
+  
   # computing conditional variance and std. residuals
-   h <- vgarch(estimates$a, estimates$A, estimates$B, eps)    # estimating conditional variances
-   std.resid <- eps/sqrt(h)                    # std. residuals
-
+  h <- vgarch(estimates$a, estimates$A, estimates$B, eps)    # estimating conditional variances
+  std.resid <- eps/sqrt(h)                    # std. residuals
+  
   # A character vector/matrix for naming parameters
   name.id <- as.character(1:ndim)
   
@@ -199,13 +197,13 @@ summary.ccc <- function(object, ...){
   # re-arranging parameter vector into a list with paramerer matrices
   para.mat <- p.mat(param, object$model, ndim)
   para.mat$R <- make.pd(para.mat$R)
-
+  
   # A character vector/matrix for naming parameters
   name.id <- as.character(1:ndim)
   namev <- diag(0, ndim, ndim)
   for(i in 1:ndim){
     for(j in 1:ndim){
-     namev[i, j] <- paste(name.id[i], name.id[j], sep="")
+      namev[i, j] <- paste(name.id[i], name.id[j], sep="")
     }
   }
   # naming parameters
@@ -225,25 +223,25 @@ summary.ccc <- function(object, ...){
   object$R <- para.mat$R[lower.tri(para.mat$R)]
   names(object$R) <- paste("R", namev[lower.tri(namev)], sep="")
   object$garch.par <- c(para.mat$a, vecA, vecB)  # estimates for conditional variance
-
+  
   # computing Jacobian and Hessian for the mean and GARCH part
   all.par <- c(object$mu, object$garch.par, object$R)
   npar <- length(all.par)       # the number of total parameters
   ja <- jacobian(func=loglik.ccc.t, x=all.par, 
-                    data=object$data, model=object$model, mode="gradient")  # using jacobian() in numDeriv
+                 data=object$data, model=object$model, mode="gradient")  # using jacobian() in numDeriv
   J <- crossprod(ja)  # information matrix
-   H <- hessian(func = loglik.ccc.t, x=all.par, 
-                 data=object$data, model=object$model, mode="hessian", method.args=list(eps=1e-5, d=1e-5))
+  H <- hessian(func = loglik.ccc.t, x=all.par, 
+               data=object$data, model=object$model, mode="hessian", method.args=list(eps=1e-5, d=1e-5))
   invH <- solve(H)
   S <- invH%*%J%*%invH
   se <- sqrt(diag(S))     
-
+  
   coef <- c(object$mu, object$garch.par, object$R)
   zstat <- all.par/se
   pval <- round(2*pnorm(-abs(zstat)), 6)
   coef <- cbind(all.par, se, zstat, pval)
   colnames(coef) <- c("Estimate", "Std. Error", "z value", "Pr(>|z|)")
-
+  
   object$coef <- coef
   
   object$convergence <- object$results$convergence    # convergence status
@@ -251,7 +249,7 @@ summary.ccc <- function(object, ...){
   object$counts <- c(object$results$outer.iter, object$results$nfuneval)   # the number of iterations
   
   object$logLik <- -tail(object$results$values, 1)  # the value of the log-like at the estimate
-
+  
   # Information Criteria
   object$AIC <- -2*object$logLik + 2*npar
   object$BIC <- -2*object$logLik + log(nobs)*npar
@@ -274,18 +272,18 @@ print.summary.ccc <- function(x, digits = max(3, getOption("digits") - 1), ...){
   
   cat("\nNumber of Obs.:", formatC(x$nobs, digits = 0), "\n")
   cat("Log-likelihood:", formatC(x$logLik, format="f", digits = 5), "\n\n")
-
+  
   cat("Information Criteria:", "\n")
   cat("  AIC:", formatC(x$AIC, format="f", digits = 3), "\n")
   cat("  BIC:", formatC(x$BIC, format="f", digits = 3), "\n")
   cat(" CAIC:", formatC(x$CAIC, format="f", digits = 3), "\n")
-
+  
   cat("\nOptimization method:", x$method, "\n")
   cat("Convergence :", x$convergence, "\n")
   cat("Iterations  :", "\n")
   cat("  Major iterations :", x$counts[1], "\n")
   cat(" Func. evaluations :", x$counts[2], "\n")
-    
+  
   invisible(x)
 }
 
